39dc8bb3ef7ce1615ee94d6530c207c2bc2e7ac2
AgustinaKravsiuk <agustinakravsiuk@gmail.com>
2023-04-05 22:20:19 -0300

    Promesas realizadas

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const data = require('./utils/data');
+const getCharById = require("./controllers/getCharById")
 
 const PORT = 3001;
 
@@ -8,16 +8,20 @@ http
 .createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     const { url } = req;
-    if(url.includes("/rickandmorty/character")) {
-        const id = Number(url.split("/").at(-1));
-        const character = data.find((char) => char.id === id);
-        if(character) {
-            res.writeHead(200, {"Content-Type" : "application/json"})
-            return res.end(JSON.stringify(character));
-        } else {
-            res.writeHead(404, {"Content-Type": "application/json"})
-            res.end(JSON.stringify({error: "Character not found"}))
-        }
+    // if(url.includes("/rickandmorty/character")) {
+    //     const id = Number(url.split("/").at(-1));
+    //     const character = data.find((char) => char.id === id);
+    //     if(character) {
+    //         res.writeHead(200, {"Content-Type" : "application/json"})
+    //         return res.end(JSON.stringify(character));
+    //     } else {
+    //         res.writeHead(404, {"Content-Type": "application/json"})
+    //         res.end(JSON.stringify({error: "Character not found"}))
+    //     }
+    // }
+    if(url.includes("/rickandmorty/character")){
+        const urlId = Number(url.split("/").at(-1))
+        getCharById(res, urlId);
     }
 })
 .listen(PORT, "localhost");
